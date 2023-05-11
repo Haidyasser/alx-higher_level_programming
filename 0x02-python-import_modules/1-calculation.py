8cf43cda5c9360487f1393a71de905f728f173d0
Haidyasser <haidyasser3@gmaail.com>
2023-05-11 22:56:54 +0300

    handle checker

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
 if __name__ == "__main__":
-    op = ['+', '-', '*', '/']
-    op_func = [add, sub, mul, div]
-    print(f"{a} {op[0]} {b} = {op_func[0](a, b)}")
-    print(f"{a} {op[1]} {b} = {op_func[1](a, b)}")
-    print(f"{a} {op[2]} {b} = {op_func[2](a, b)}")
-    print(f"{a} {op[3]} {b} = {op_func[3](a, b)}")
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
