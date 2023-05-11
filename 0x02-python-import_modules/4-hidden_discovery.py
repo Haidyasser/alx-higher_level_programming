acabf1b50577d5d572ecd5178fbc56dabcccfee6
Haidyasser <haidyasser3@gmaail.com>
2023-05-12 00:14:27 +0300

    add4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3.8
 import hidden_4
 
+
 def print_names():
-	for name in sorted(dir(hidden_4)):
-		if name.startswith("__"):
-			print(name)
+    for name in sorted(dir(hidden_4)):
+        if name.startswith("__"):
+            print(name)
+
 
 if __name__ == '__main__':
     print_names()
