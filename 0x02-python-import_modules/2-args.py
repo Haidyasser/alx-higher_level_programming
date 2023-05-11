1821ed23e079053b49218b75d49beea094e93ee5
Haidyasser <haidyasser3@gmaail.com>
2023-05-11 23:44:48 +0300

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,13 @@
 import sys
 if __name__ == "__main__":
     cnt = len(sys.argv)
-    print("{} arguments".format(cnt - 1), end="")
+    print("{} argument".format(cnt - 1), end="")
     if cnt == 1:
-        print(".")
-    else:
+        print("s.")
+    elif cnt == 2:
         print(":")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("s:")
         for i in range(1, cnt):
             print("{}: {}".format(i, sys.argv[i]))
