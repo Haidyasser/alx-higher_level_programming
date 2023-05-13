b9ca33c9dd7c0406b3c534e3b9c5c586939854ac
Haidyasser <haidyasser3@gmaail.com>
2023-05-13 23:25:47 +0300

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,6 +5,6 @@ def no_c(my_string):
     ans = ""
     if my_string:
         for c in my_string:
-            if c != 'c':
+            if c != 'c' and c != 'C':
                 ans += c
     return ans
