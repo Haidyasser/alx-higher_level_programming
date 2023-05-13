25b46a1dc76a91be4aef472990316d58606e3102
Haidyasser <haidyasser3@gmaail.com>
2023-05-13 22:12:33 +0300

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def def replace_in_list(mylist, idx, element):
+def replace_in_list(mylist, idx, element):
     if idx < 0:
         return mylist
     if idx >= len(mylist):
