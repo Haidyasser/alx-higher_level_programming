e85e88b6ceaf4caf2f54c5c2327c7007c59851c5
Haidyasser <haidyasser3@gmaail.com>
2023-05-16 18:45:50 +0300

    task7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     tmp = []
-    for i in range(max(len(tuple_a), len(tuple_b), 2)):
+    for i in range(max(len(tuple_a), len(tuple_b), 2)) and len(tmp) < 2:
         e = 0
         if i < len(tuple_a):
             e += tuple_a[i]
