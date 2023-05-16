0c38a8aba55824d75a0a115fcdb3e8df77a2b7a0
Haidyasser <haidyasser3@gmaail.com>
2023-05-16 20:28:51 +0300

    task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+    ans = []
+    for i in my_list:
+        if i % 2 == 1:
+            ans.append(False)
+        else:
+            ans.append(True)
+    return ans
