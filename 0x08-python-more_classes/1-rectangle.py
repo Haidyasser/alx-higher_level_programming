0732221b7261954ad29d011c818b1d85a40bc79c
Haidyasser <haidyasser3@gmaail.com>
2024-03-09 19:48:05 +0200

    handle code style

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,11 +7,11 @@ class Rectangle:
     def __init__(self, width=0, height=0) -> None:
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -31,4 +31,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
