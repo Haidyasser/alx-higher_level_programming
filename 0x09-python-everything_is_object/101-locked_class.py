f185201c2320b9f163356e3aed80086111a8bc52
Haidyasser <haidyasser3@gmail.com>
2024-09-11 14:51:11 +0300

    define a locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Defines a locked class"""
 class LockedClass:
+    """Class that is have only fist_name attribute"""
     __slots__ = ['first_name']
 
     def __init__(self, first_name=None):
