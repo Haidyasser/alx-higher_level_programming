b7946daf96f4dc023cedded90f095ec15c8dfeb9
Haidyasser <haidyasser3@gmail.com>
2024-09-12 15:23:33 +0300

    pycodestyle lookup method

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-"""module of methond the list of available attributes and methods of an object."""
+"""
+module of methond the list of available
+attributes and methods of an object.
+"""
 
 
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object."""
+    """
+    Returns the list of available attributes
+    and methods of an object."""
     return dir(obj)
